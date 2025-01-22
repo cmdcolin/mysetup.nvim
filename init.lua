4d91eb89c9002e803f5ad66195dad047399555bd
Colin <colin.diesh@gmail.com>
2025-01-22 15:32:54 -0500

    Try out blink

--- init.lua
+++ init.lua
@@ -770,97 +770,69 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'saghen/blink.cmp',
 
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    -- use a release tag to download pre-built binaries
+    version = '*',
+    signature = { enabled = true },
+    build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+      'L3MON4D3/LuaSnip',
+      version = 'v2.*',
+    },
+    opts = {
+      snippets = { preset = 'luasnip' },
+
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = {
+        preset = 'super-tab',
+      },
+      completion = {
+        menu = {
+          auto_show = true,
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+        },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
         },
       },
-      'saadparwaiz1/cmp_luasnip',
 
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = {
+          'snippets',
+          'lsp',
+          'path',
+          'buffer',
+        },
+      },
     },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-
-        -- For an understanding of why these mappings were
-        -- chosen, you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          ['<C-l>'] = cmp.mapping.confirm { select = true },
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
-        },
-        sources = {
-          {
-            name = 'lazydev',
-            -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-            group_index = 0,
-          },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
+    opts_extend = { 'sources.default' },
   },
 
   { -- You can easily change to a different colorscheme.
@@ -1010,10 +982,10 @@ local v = {
   ls.parser.parse_snippet('pa', '/**\n* #method\n*/'),
   ls.parser.parse_snippet('pa', '/**\n* #volatile\n*/'),
   ls.parser.parse_snippet('ps', 'const {$1} = self'),
-  ls.parser.parse_snippet('cl', 'console.log({$1})'),
-  ls.parser.parse_snippet('cl', 'console.log($1)'),
-  ls.parser.parse_snippet('cl', 'console.log("$1")'),
-  ls.parser.parse_snippet('cl', 'console.log(e)'),
+  ls.parser.parse_snippet('cll', 'console.log({$1})'),
+  ls.parser.parse_snippet('cll', 'console.log($1)'),
+  ls.parser.parse_snippet('cll', 'console.log("$1")'),
+  ls.parser.parse_snippet('cll', 'console.log(e)'),
   ls.parser.parse_snippet('wa', '"language":["$1"],'),
   ls.parser.parse_snippet('wa', '"tags":["$1"],'),
   ls.parser.parse_snippet('wa', '"pub":{"doi":""},'),
